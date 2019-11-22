379ce77753fe94022ed45255767336438b87f48d
Artem <artger@ukr.net>
2019-11-22 12:33:36 +0200

    version 1

--- stack.py
+++ stack.py
@@ -1,9 +1,15 @@
+'''Module'''
 class Stack:
+    '''
+    Stack
+    '''
     def __init__(self):
-        ...
+        self.items = []
 
     def push(self, value):
-        pass
+        '''Add value in the list'''
+        self.items.append(value)
 
     def pop(self):
-        return 0
+        '''Return last element'''
+        return self.items.pop()
