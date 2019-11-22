26054cb36a510c42d82243c18d097a91db44969b
logotip <logotip123@yahoo.com>
2019-11-22 10:48:26 +0200

    v 1.0 logotip1992@gmail.com

--- stack.py
+++ stack.py
@@ -1,9 +1,18 @@
+"""stack module"""
+
+
 class Stack:
+    """stack class"""
     def __init__(self):
-        ...
+        self.array = []
 
     def push(self, value):
-        pass
+        """push to stack"""
+        self.array.append(value)
 
     def pop(self):
-        return 0
+        """pop from stack"""
+        if not self.array:
+            raise IndexError
+        result = self.array.pop()
+        return result
