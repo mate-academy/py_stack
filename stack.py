c040b8772ae1139fc11009f93a14506e0e6708be
vlysenok <tina.lysenok@gmail.com>
2019-11-22 13:11:26 +0200

    hw5_py_stack

--- stack.py
+++ stack.py
@@ -1,9 +1,34 @@
+"""
+stack module
+"""
+
+
 class Stack:
+    """
+    Classic stack based on one of the base Python containers.
+    """
+
     def __init__(self):
-        ...
+        self.__storage = []
 
     def push(self, value):
-        pass
+        """
+        Added value to stack.
+        :param value: Any
+        :return: None
+        """
+        self.__storage.append(value)
 
     def pop(self):
-        return 0
+        """
+        Remove value from stack.
+        :return: Any
+        """
+        return self.__storage.pop()
+
+    def is_empty(self):
+        """
+        Check if stack is empty.
+        :return: bool
+        """
+        return len(self.__storage) == 0
