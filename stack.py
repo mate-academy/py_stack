323c741f18545c95207175e9859c7bbfe2c3e83b
zaikovlad95@gmail.com <Makepeacenotwar1995>
2019-11-29 18:20:13 +0200

    Stack class realization.

--- stack.py
+++ stack.py
@@ -1,9 +1,17 @@
+"""Stack module."""
+
+
 class Stack:
+    """Stack class."""
+
     def __init__(self):
-        ...
+        """Initialize an empty list."""
+        self.stack = []
 
     def push(self, value):
-        pass
+        """Append a value to stack"""
+        self.stack.append(value)
 
     def pop(self):
-        return 0
+        """:returns the last added value of stack"""
+        return self.stack.pop()
